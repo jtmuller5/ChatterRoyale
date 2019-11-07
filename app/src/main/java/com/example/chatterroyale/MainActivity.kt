c33bf3698d76acbf51805266d063601749084ab0
jtmuller5 <47997351+jtmuller5@users.noreply.github.com>
2019-11-06 20:20:06 -0600

    Updating Icons

--- app/src/main/java/com/example/chatterroyale/MainActivity.kt
+++ app/src/main/java/com/example/chatterroyale/MainActivity.kt
@@ -32,12 +32,15 @@ class MainActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
+
+        navView.itemIconTintList = null
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send
+                R.id.nav_home, R.id.nav_chatter, R.id.nav_perks,
+                R.id.nav_profile, R.id.nav_settings
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
